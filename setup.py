8201488b0f39a8ed87a28d96e5abb6d15c49c4e0
Adastram <105358415+Adastram1@users.noreply.github.com>
2024-02-28 20:24:18 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.1.1'
+DESCRITPION = 'An "easier" version of the pystray package'
+LONG_DESCRIPTION = 'A package for adding a system tray icon, based on pystray, this package is an "easier" version of pystray to manipulate'
+
+# Setting up
+setup(
+    name="tray_manager",
+    version=VERSION,
+    author="Adastram (Github : Adastram1)",
+    author_email="",
+    description=DESCRITPION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    requires=['pystray', 'typing', 'types', 'threading', 'pillow', 'enum'],
+    keywords=['python', 'manager', 'system tray', 'pystray'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
