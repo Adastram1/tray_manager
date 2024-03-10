9e33f3058c6207fedeedde1e090f32eeb4e7fc00
Adastram <105358415+Adastram1@users.noreply.github.com>
2024-03-10 01:18:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,10 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3",
+        "(LGPLv3)",
         "Operating System :: Microsoft :: Windows",
-        "OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
     ]
 )
